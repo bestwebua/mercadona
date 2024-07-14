60639fa5c41e3f21c9505488f6b30ddf5e195a64
Vladislav Trotsenko <admin@bestweb.com.ua>
2024-07-14 18:27:03 +0200

    Feature/Implement discount resolver (#4)
    * Implemented Mercadona::DiscountResolver, tests
    * Implemented Mercadona::DiscountType::Base stub

--- lib/mercadona/core.rb
+++ lib/mercadona/core.rb
@@ -4,6 +4,7 @@ module Mercadona
   require_relative '../mercadona/version'
   require_relative '../mercadona/order_item'
   require_relative '../mercadona/amount_formatter'
+  require_relative '../mercadona/discount_type/base'
   require_relative '../mercadona/discount_resolver'
   require_relative '../mercadona/checkout'
 end
--- spec/mercadona/discount_resolver_spec.rb
+++ spec/mercadona/discount_resolver_spec.rb
@@ -2,8 +2,16 @@
 
 RSpec.describe Mercadona::DiscountResolver do
   describe '.call' do
-    subject(:discount_resolver) { described_class.call(42) }
+    subject(:discount_resolver) { described_class.call(discount_rules, order_item, quantity) }
 
-    it { is_expected.to be_nil }
+    let(:quantity) { 1 }
+    let(:order_item) { create(:order_item) }
+    let(:order_item_rule) { 'some_rules' }
+    let(:discount_rules) { { order_item.product_code => [:base, 'some_rules'] } }
+
+    it 'calls target discount class mapped by order item product_code' do
+      expect(Mercadona::DiscountType::Base).to receive(:call).with([order_item_rule], order_item, quantity)
+      discount_resolver
+    end
   end
 end

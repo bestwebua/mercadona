b4930b635df2fe310b10fec52c28719db80acc9f
Vladislav Trotsenko <admin@bestweb.com.ua>
2024-07-15 15:13:12 +0200

    Feature/Implement discount base class
    * Added Mercadona::Discount::Base, tests
    * Added Mercadona::Error::Runtime, tests

--- lib/mercadona/core.rb
+++ lib/mercadona/core.rb
@@ -2,10 +2,11 @@
 
 module Mercadona
   require_relative '../mercadona/version'
+  require_relative '../mercadona/error/runtime'
   require_relative '../mercadona/entity/base'
   require_relative '../mercadona/entity/discount_rule'
   require_relative '../mercadona/entity/order_item'
   require_relative '../mercadona/amount_formatter'
-  require_relative '../mercadona/discount_type/base'
+  require_relative '../mercadona/discount/base'
   require_relative '../mercadona/checkout'
 end
